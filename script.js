2c414f7a56082bfe829eab84fc16259aeea96519
Navya <88023981+navysai1419@users.noreply.github.com>
2024-02-23 11:34:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 //your JS code here. If required.
+document.addEventListner("submit",function(event){
+	event.preventDefault();
+
+    const username=document.getElementById("username");
+	const password=document.getElementById("password");
+	const rememberme=document.getElementById("checkbox").checked;
+
+	if(rememberme)
+	{
+		localStorage.setItem("username",username);
+		localStorage.setItem("password",password);
+		
+	}
+	else
+	{
+		localStorage.removeItem("username");
+		localStorage.removeItem("password");	
+	}
+	alert("Logged in as: " + username);
+	const storedUsername = localStorage.getItem("username");
+    const storedPassword = localStorage.getItem("password");
+
+  // If there are stored credentials, fill the form
+  if (storedUsername && storedPassword) {
+    document.getElementById("username").value = storedUsername;
+    document.getElementById("password").value = storedPassword;
+    document.getElementById("rememberMe").checked = true;
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+						 
+	)
